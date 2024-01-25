961aef3741d6bb6f162099e8a3ee0003aad6acb5
NRG55 <maloj_1980@yahoo.fr>
2024-01-25 00:00:25 +0000

    Set 'hover' effect, when a mouse eneters a div (cell of the grid) the div changes the color of it. Added a button that send the user a popup asking for the number of squares per side for the new grid and generates a new grid in the same space as before

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,26 +1,48 @@
 const container = document.querySelector('.container');
+const btnReset = document.querySelector('.btnReset');
+const gridSize = 20;
   
 function changeGridSize(cellQty){
-
     for (let i = 0; i < cellQty; i++){
-
-        const gridDiv = document.createElement('div'); 
+        const gridDiv = document.createElement('div');
 
         gridDiv.classList.add('gridRow');        
         
         for (let j = 0; j < cellQty; j++){
-
             const childGridDiv = document.createElement('div');
-            const cellSize = 600 / cellQty; 
-            
+            const cellSize = 600 / cellQty;  
+
             childGridDiv.classList.add('childGridDiv');
             childGridDiv.style.width = `${cellSize}px`;
             childGridDiv.style.height = `${cellSize}px`;
-            gridDiv.appendChild(childGridDiv); 
+            gridDiv.appendChild(childGridDiv);             
         }
-
-        container.appendChild(gridDiv);     
+        container.appendChild(gridDiv);         
     }
+    const cells = document.querySelectorAll('.childGridDiv');
+
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {        
+        cell.style.background = 'black';
+        })
+    })    
 }
 
-changeGridSize(4);
+function emptyDivContainer (parent){
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    } 
+    return;
+}
+
+btnReset.addEventListener('click', () => {    
+    let userNumber = Number(prompt('Please enter the number for a new size of the grid'));
+    emptyDivContainer(container);
+    return changeGridSize(userNumber);
+})
+
+changeGridSize(gridSize);
+
+
+
+
