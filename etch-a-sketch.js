61fae2df56e039150668a44759886a9bb0c0b915
NRG55 <maloj_1980@yahoo.fr>
2024-01-25 20:36:25 +0000

    Added - Erase, Black and Color buttons witn a listeners, Also rather than a color change from black to white, each interaction randomize the square's RGB value entirely

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,9 @@
 const container = document.querySelector('.container');
 const btnReset = document.querySelector('.btnReset');
-const gridSize = 20;
+const btnColor = document.querySelector('.btnColor');
+const btnBlack = document.querySelector('.btnBlack');
+const btnErase = document.querySelector('.btnErase');
+const gridSize = 50;
   
 function changeGridSize(cellQty){
     for (let i = 0; i < cellQty; i++){
@@ -10,22 +13,20 @@ function changeGridSize(cellQty){
         
         for (let j = 0; j < cellQty; j++){
             const childGridDiv = document.createElement('div');
-            const cellSize = 600 / cellQty;  
-
+            const cellSize = 600 / cellQty;
+          
             childGridDiv.classList.add('childGridDiv');
             childGridDiv.style.width = `${cellSize}px`;
             childGridDiv.style.height = `${cellSize}px`;
+            
+            childGridDiv.addEventListener('mouseover', () => {        
+            childGridDiv.style.background = 'black';            
+            })
+
             gridDiv.appendChild(childGridDiv);             
         }
         container.appendChild(gridDiv);         
-    }
-    const cells = document.querySelectorAll('.childGridDiv');
-
-    cells.forEach((cell) => {
-        cell.addEventListener('mouseover', () => {        
-        cell.style.background = 'black';
-        })
-    })    
+    }   
 }
 
 function emptyDivContainer (parent){
@@ -35,10 +36,57 @@ function emptyDivContainer (parent){
     return;
 }
 
+function createRandomColor(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    console.log({r, g, b});
+        return `rgb(${r}, ${g}, ${b})`;
+}
+
 btnReset.addEventListener('click', () => {    
-    let userNumber = Number(prompt('Please enter the number for a new size of the grid'));
+    let userNumber = Number(prompt('Please enter a number of squares per side up to 100'));
+   
+    if(!userNumber){                
+        return;
+    }
+
+    while (userNumber > 100){
+        userNumber = Number(prompt('Please enter a smaller number of squares per side up to 100'));
+    }    
+    
     emptyDivContainer(container);
     return changeGridSize(userNumber);
+})
+
+btnColor.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.childGridDiv');
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseover', () => {
+            cells[i].style.background = createRandomColor();
+        })
+    }
+})
+
+btnBlack.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.childGridDiv');
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseover', () => {
+            cells[i].style.background = 'black';
+        })
+    }
+})
+
+btnErase.addEventListener('click', () => {
+    let cells = document.querySelectorAll('.childGridDiv');
+
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mouseover', () => {
+            cells[i].style.background = 'white';
+        })
+    }
 })
 
 changeGridSize(gridSize);
